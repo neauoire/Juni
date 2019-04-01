4607bff2579968b8d53149adf044848050baa298
Devine Lu Linvega <aliceffekt@gmail.com>
2019-04-01 11:39:11 +0900

    Don't overwrite Keybow function

--- juni.lua
+++ juni.lua
@@ -1,8 +1,6 @@
--- require "keybow"
+require "keybow"
 
 local layer = 0
-
-local testing = {0,1,2,3}
 
 template = {
     {},
@@ -31,7 +29,7 @@ function get_char(key,layer)
     return template[key+1][layer+1]
 end
 
-function set_key(ch,pressed)
+function input(ch,pressed)
     keybow.set_key(ch, pressed)
 end
 
@@ -44,61 +42,61 @@ end
 -- Top --
 
 function handle_key_11(pressed)
-    set_key(get_char(11,layer), pressed)
+    input(get_char(11,layer), pressed)
     set_layer(8, pressed)
 end
 
 function handle_key_08(pressed)
-    set_key(get_char(8,layer), pressed)
+    input(get_char(8,layer), pressed)
     set_layer(7, pressed)
 end
 
 function handle_key_05(pressed)
-    set_key(get_char(5,layer), pressed)
+    input(get_char(5,layer), pressed)
     set_layer(6, pressed)
 end
 
 function handle_key_02(pressed)
-    set_key(get_char(2,layer), pressed)
+    input(get_char(2,layer), pressed)
     set_layer(5, pressed)
 end
 
 -- Mid --
 
 function handle_key_10(pressed)
-    set_key(get_char(10,layer), pressed)
+    input(get_char(10,layer), pressed)
     set_layer(4, pressed)
 end
 
 function handle_key_07(pressed)
-    set_key(get_char(7,layer), pressed)
+    input(get_char(7,layer), pressed)
     set_layer(3, pressed)
 end
 
 function handle_key_04(pressed)
-    set_key(get_char(4,layer), pressed)
+    input(get_char(4,layer), pressed)
     set_layer(2, pressed)
 end
 
 function handle_key_01(pressed)
-    set_key(get_char(1,layer), pressed)
+    input(get_char(1,layer), pressed)
     set_layer(1, pressed)
 end
 
 -- Low --
 
 function handle_key_09(pressed)
-    set_key("0", pressed)
+    input("0", pressed)
 end
 
 function handle_key_06(pressed)
-    set_key("0", pressed)
+    input("0", pressed)
 end
 
 function handle_key_03(pressed)
-    set_key("0", pressed)
+    input("0", pressed)
 end
 
 function handle_key_00(pressed)
-    set_key("0", pressed)
+    input("0", pressed)
 end
